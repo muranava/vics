2ecfe553074ab03c771a3a48f36c1117882f1936
Stein Fletcher <steinfletcher@gmail.com>
2016-02-04 23:04:39 +0000

    Add respond.js to bower deps

--- web-client/test/karma.conf.js
+++ web-client/test/karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -31,6 +32,8 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/lodash/lodash.js',
+      'bower_components/respond/dest/respond.src.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
