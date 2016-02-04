2a029b81fd06ac9ab0c50ad661a3dd077bd5c9cc
Stein Fletcher <steinfletcher@gmail.com>
2016-02-04 10:42:14 +0000

    Add client scaffolding. This will be replaced in a future commit

---
+++ web-client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name canvassCliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the canvassCliApp
+ */
+angular.module('canvassCliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
