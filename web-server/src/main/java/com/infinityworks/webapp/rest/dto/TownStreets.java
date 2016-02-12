eab5b25b1abeb35577de8a6bc6509ca4d4eec2cb
Stein Fletcher <steinfletcher@gmail.com>
2016-02-12 15:06:02 +0000

    Remove sun class imported by error

--- web-server/src/main/java/com/infinityworks/webapp/rest/dto/TownStreets.java
+++ web-server/src/main/java/com/infinityworks/webapp/rest/dto/TownStreets.java
@@ -1,13 +1,12 @@
 package com.infinityworks.webapp.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
-import com.sun.istack.internal.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
