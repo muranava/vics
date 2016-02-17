e5c5c42ae6c4739f600b45e8d2dd693503970c46
Stein Fletcher <steinfletcher@gmail.com>
2016-02-17 10:40:27 +0000

    Changes voter to voters after api change

--- web-server/src/test/java/com/infinityworks/webapp/testsupport/Fixtures.java
+++ web-server/src/test/java/com/infinityworks/webapp/testsupport/Fixtures.java
@@ -1,30 +1,8 @@
 package com.infinityworks.webapp.testsupport;
 
-import com.infinityworks.webapp.domain.Role;
-import com.infinityworks.webapp.domain.User;
-import com.infinityworks.webapp.domain.Ward;
 import com.infinityworks.webapp.rest.dto.Street;
-import com.infinityworks.webapp.testsupport.builder.UserBuilder;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static com.infinityworks.webapp.testsupport.builder.WardBuilder.ward;
 
 public class Fixtures {
-
-    public static User aUser() {
-        Set<Ward> wards = new HashSet<>();
-        wards.add(ward().withWardName("a").build());
-        wards.add(ward().withWardName("b").build());
-
-        User user = new UserBuilder().build();
-        user.setUsername("Jon");
-        user.setPasswordHash("4325");
-        user.setRole(Role.USER);
-        user.setWards(wards);
-        return user;
-    }
 
     public static Street kirbyRoad() {
         return new Street("Kirby Road", "Coventry", "Northern Quarter", "");
