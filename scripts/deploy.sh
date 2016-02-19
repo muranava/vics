7d77e3f101ed26f1cda34c227ebdc21361994c47
Stein Fletcher <steinfletcher@gmail.com>
2016-02-19 12:44:55 +0000

    Add server jar to packaged app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,5 @@
 
 host=134.213.209.158
 
-scp -i ~/.ssh/vote_leave.pub ../web-server/target/web-server-0.0.1-SNAPSHOT.jar forge@134.213.209.158:/home/forge/projects/canvass
-
-scp -r -i ~/.ssh/vote_leave.pub ../web-client/dist forge@134.213.209.158:/home/forge/projects/canvass
-scp -r -i ~/.ssh/vote_leave.pub run-web-server.sh forge@134.213.209.158:/home/forge/projects/canvass
+scp -r -i ~/.ssh/vote_leave.pub ../dist forge@134.213.209.158:/home/forge/projects/canvass
 
