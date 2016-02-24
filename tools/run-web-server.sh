b817d602d48da9fbb8b3d04ca7d48aec92689d65
Stein Fletcher <steinfletcher@gmail.com>
2016-02-24 09:35:22 +0000

    remove ward code from canvass view

--- tools/run-web-server.sh
+++ tools/run-web-server.sh
@@ -8,8 +8,8 @@ APP_JAR=$DIST_DIR/web-server-0.0.1.jar
 PORT=18080
 CONTEXT_PATH=/api/canvass
 
-DATA_SOURCE_URL=jdbc:postgresql://localhost:5432/canvass
+DATA_SOURCE_URL=jdbc:postgresql://localhost:5432/canvassapp
 DATA_SOURCE_USER=forge
 DATA_SOURCE_PASSWD=
 
-java -Dserver.port=$PORT -Dserver.context-path=$CONTEXT_PATH -Dspring.datasource.username=$DATA_SOURCE_USER -Dspring.datasource.password=$DATA_SOURCE_PASSWD -Dspring.datasource.url=$DATA_SOURCE_URL -jar $APP_JAR
+nohup java -Dlogging.level.=ERROR -Dserver.port=$PORT -Dserver.context-path=$CONTEXT_PATH -Dspring.datasource.username=$DATA_SOURCE_USER -Dspring.datasource.password=$DATA_SOURCE_PASSWD -Dspring.datasource.url=$DATA_SOURCE_URL -jar $APP_JAR
