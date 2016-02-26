0a2ed1999d3f196881040ac5d852fbcf53146477
Stein Fletcher <steinfletcher@gmail.com>
2016-02-26 23:17:49 +0000

    Move properties out to env

--- tools/run-web-server.sh
+++ tools/run-web-server.sh
@@ -8,8 +8,8 @@ APP_JAR=$DIST_DIR/web-server-0.0.1.jar
 PORT=18080
 CONTEXT_PATH=/api/canvass
 
-DATA_SOURCE_URL=jdbc:postgresql://localhost:5432/canvassapp
-DATA_SOURCE_USER=forge
+DATA_SOURCE_URL=$DB_URL
+DATA_SOURCE_USER=$DB_USER
 DATA_SOURCE_PASSWD=$DB_PASSWD
 
 nohup java -d64 -Xms512m -Xmx4g -Dlogging.level.=ERROR -Dlogging.level.com.infinityworks=ERROR -Dserver.port=$PORT -Dserver.context-path=$CONTEXT_PATH -Dspring.datasource.username=$DATA_SOURCE_USER -Dspring.datasource.password=$DATA_SOURCE_PASSWD -Dspring.datasource.url=$DATA_SOURCE_URL -jar $APP_JAR
