afbd6ad28f59682dc8ff59c0305f54ec6c823320
tapatron <steinfletcher@gmail.com>
2016-02-27 02:03:09 +0000

    Update install-db.sh

--- tools/install-db.sh
+++ tools/install-db.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-ProductionEnvironment=NO
-
-if [ "$ProductionEnvironment" = "YES" ] ; then
-    exit 0
-else
-    echo "Creating database and tables..."
-    psql -U postgres -f db/create.sql --quiet
-    echo "Creating constituencies..."
-    psql -U postgres -f db/constituencies.sql --quiet
-    echo "Creating wards..."
-    psql -U postgres -f db/wards.sql --quiet
-    echo "Creating users..."
-    psql -U postgres -f db/users.sql --quiet
-    echo "Done."
-fi
+echo "Creating database and tables..."
+psql -U postgres -f db/create.sql --quiet
+echo "Creating constituencies..."
+psql -U postgres -f db/constituencies.sql --quiet
+echo "Creating wards..."
+psql -U postgres -f db/wards.sql --quiet
+echo "Creating users..."
+psql -U postgres -f db/users.sql --quiet
+echo "Done."
