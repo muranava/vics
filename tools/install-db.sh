79cc54484704185192d8826ad4e35b5ea8436b55
Stein Fletcher <steinfletcher@gmail.com>
2016-02-27 21:51:27 +0000

    Restructure dist

--- tools/install-db.sh
+++ tools/install-db.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Creating database and tables..."
-psql -U postgres -f db/create.sql --quiet
+psql -U postgres -f create.sql --quiet
 echo "Creating constituencies..."
-psql -U postgres -f db/constituencies.sql --quiet
+psql -U postgres -f constituencies.sql --quiet
 echo "Creating wards..."
-psql -U postgres -f db/wards.sql --quiet
+psql -U postgres -f wards.sql --quiet
 echo "Creating users..."
-psql -U postgres -f db/users.sql --quiet
+psql -U postgres -f users.sql --quiet
 echo "Done."
