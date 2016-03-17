54f02c65fa0c16703c106bbba39e40ec4e0e12c6
Stein Fletcher <steinfletcher@gmail.com>
2016-03-17 17:10:52 +0000

    Update text

--- web-client/app/scripts/controller/parentController.js
+++ web-client/app/scripts/controller/parentController.js
@@ -2,25 +2,27 @@
  * Root controller of the application
  */
 angular
-    .module('canvass')
-    .controller('parentController', function ($location, $scope, authService, $rootScope) {
+  .module('canvass')
+  .controller('parentController', function ($location, $scope, authService, $rootScope) {
 
-      function resetMenu() {
-        // Added to enforce menus are removed on logout
-        if(!$scope.$$phase) {
-          $scope.$apply();
-        }
+    function resetMenu() {
+      // Added to enforce menus are removed on logout
+      if (!$scope.$$phase) {
+        $scope.$apply();
       }
-      resetMenu();
+    }
 
-      $scope.logout = function() {
-        authService.logout()
-          .finally(function() {
-            $location.path('/login');
+    resetMenu();
+
+    $scope.logout = function () {
+      authService.logout()
+        .finally(function () {
+          $location.path('/login');
+          _.defer(function () {
             resetMenu();
           });
-        $rootScope.currentUser = null;
-      };
+        });
+      $rootScope.currentUser = null;
+    };
 
-
-    });
+  });
