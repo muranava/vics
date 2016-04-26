f91bdeb9f4c0606861b5cdc7afbca3a478293093
stein <steinfletcher@gmail.com>
2016-04-26 11:51:30 +0100

    Update staging and prod config

--- web-server/scripts/createApplication.sh
+++ web-server/scripts/createApplication.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copies the deployment artefacts to the deployment
+# Copies the deployment artifacts to the deployment
 # and initialise as a linux service (supported by Spring boot by default
 # see https://docs.spring.io/spring-boot/docs/current/reference/html/deployment-install.html#deployment-service)
 
