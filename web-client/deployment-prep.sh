cc2b862188ca100ca34c3a71755027b63d00c61b
stein <steinfletcher@gmail.com>
2016-05-18 15:20:59 +0100

    Update deployment config

--- web-client/deployment-prep.sh
+++ web-client/deployment-prep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=42
+VERSION=43
 ELB_DEV="https://WebApp-57219498.eu-west-1.elb.amazonaws.com:18080/api/canvass"
 ELB_STAGING="https://WebApp-2103587711.eu-west-1.elb.amazonaws.com:18080/api/canvass"
 ELB_PROD="https://WebApp-731548696.eu-west-1.elb.amazonaws.com:18080/api/canvass"
@@ -16,4 +16,3 @@ cp -r ../dist/client ../dist/client-prod-$VERSION
 sed -i '' "s;CANVASS_API_ELB_ENDPOINT;$ELB_DEV;g" ../dist/client-dev-$VERSION/scripts/scripts.*.js
 sed -i '' "s;CANVASS_API_ELB_ENDPOINT;$ELB_STAGING;g" ../dist/client-staging-$VERSION/scripts/scripts.*.js
 sed -i '' "s;CANVASS_API_ELB_ENDPOINT;$ELB_PROD;g" ../dist/client-prod-$VERSION/scripts/scripts.*.js
-
